4b7c3b507db9bc9f3b1ae5c19b2cee50af1d871c
sergey.balaboskin <sergey.balaboskin@just-eat.com>
2017-04-19 14:59:23 +0100

    ProcessorLog

--- src/Dream.Space.Data/Entities/Companies/Company.cs
+++ src/Dream.Space.Data/Entities/Companies/Company.cs
@@ -50,5 +50,6 @@ namespace Dream.Space.Data.Entities.Companies
         public int SectorId { get; set; }
         public int IndustryId { get; set; }
         public bool SP500 { get; set; }
+        public bool IsIndex { get; set; }
     }
 }
---
+++ src/Dream.Space.Data/Entities/Logs/ProcessorLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dream.Space.Data.Entities.Logs
+{
+    public class ProcessorLog
+    {
+        public int Id { get; set; }
+        public DateTime Logged { get; set; }
+        public string Level { get; set; }
+        public string Message { get; set; }
+        public string Processor { get; set; }
+        public string JobType { get; set; }
+        public string JobState { get; set; }
+        public string Exception { get; set; }
+        public int JobId { get; set; }
+    }
+}
